9e7decf4181144ec31cca35bae319ee28516c911
User <michaelm810129@gmail.com>
2023-10-04 09:58:44 +1030

    style and structure changes to HomePage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   const styles = {
     text: {
       textAlign: "center",
-      fontSize:25,
+      fontSize: 25,
       padding: 0,
     },
     title: {
@@ -13,35 +13,33 @@ export default function Home() {
       fontFamily: "Abril Fatface, cursive",
       textAlign: "center",
       fontSize: 50,
-    
     },
-    image: {
-      display: "block",
-      marginLeft: "auto",
-      marginRight: "auto",
-      height: "200px",
-      width: "200px",
-      // height: 350,
-      borderRadius: "50%",
-      border: "solid",
-    },
+    // image: {
+    //   display: "block",
+    //   marginLeft: "auto",
+    //   marginRight: "auto",
+    //   height: "200px",
+    //   width: "200px",
+    //   // height: 350,
+    //   borderRadius: "50%",
+    //   border: "solid",
+    // },
   };
   const image = process.env.PUBLIC_URL + "/profile.png";
   return (
     <Container>
       <section>
-        <div  >
-          <h3 className="homeWelcome" style={styles.title}>Welcome.</h3>
+        <div className="home">
+          <div className="home--content">
+            <h3 className="home--title">Welcome.</h3>
+
+            <p className="my-auto home--Text">
+              My name is Michael Monaghan and I invite you to browse my
+              portfolio.
+            </p>
+          </div>
+          <img className="m-auto bioImg" src={image} alt="profile" />
         </div>
-        <p style={styles.text} className="my-auto welcomeText">
-            My name is Michael Monaghan and I invite you to browse my portfolio.
-          </p>
-        <img
-          className="m-auto bioImg"
-          style={styles.image}
-          src={image}
-          alt="profile"
-        ></img>
       </section>
     </Container>
   );
